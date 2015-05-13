901a85e594573c8ab290b1dee732a3d9f21417aa
Alex Urcera <Alex Urcera@Alex>
2015-05-13 18:30:31 +0200

    commit2

--- src/clasesanidadas/Ventana.java
+++ src/clasesanidadas/Ventana.java
@@ -1,5 +1,7 @@
 package clasesanidadas;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
@@ -15,19 +17,38 @@ public class Ventana {
     JFrame marco;
     JPanel panel;
     JButton boton;
+    JButton botonR;
+    JButton botonG;
+    JButton botonB;
+    FlowLayout dis = new FlowLayout();
 
     public void amosarVentana() {
         marco = new JFrame("   ventanas anidadas   ");
         panel = new JPanel();
         boton = new JButton("Boton");
+        botonR=new JButton("ROjo");
+        botonG=new JButton("Verde");
+        botonB=new JButton("Azul");
         marco.setSize(500, 500);
         panel.setSize(500, 500);
         boton.setSize(100, 200);
-        panel.setLayout(null);
+        panel.setLayout(dis);
+        panel.add(botonB);
+        panel.add(botonR);
+        panel.add(botonG);
+        
         panel.add(boton);
         marco.add(panel);
         boton.addActionListener(new Event());
-//        marco.pack();
+        botonR.addActionListener(new Erojo());
+        botonB.addActionListener(new Eazul());
+        botonG.addActionListener(new Everde());
+        marco.add(panel);
+
+        
+        
+        
+       
         marco.setVisible(true);
         marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -49,5 +70,44 @@ public class Ventana {
 
         }
 
+    }
+    class Erojo implements ActionListener{
+
+        public Erojo() {
+            panel.setBackground(Color.RED);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Erojo roj=new Erojo();
+                    
+        }
+        
+    }
+    class Eazul implements ActionListener{
+
+        public Eazul() {
+            panel.setBackground(Color.BLUE);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Eazul blu=new Eazul();
+                    
+        }
+        
+    }
+    class Everde implements ActionListener{
+
+        public Everde() {
+            panel.setBackground(Color.GREEN);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Everde green=new Everde();
+                    
+        }
+        
     }
 }
